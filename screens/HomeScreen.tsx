ed7e95390c1101c9b270b70486463aab125d3555
all123all <allyson2308h@gmail.com>
2021-03-19 20:29:06 -0300

    no modules

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Home JUST MAKE SENSE!</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
