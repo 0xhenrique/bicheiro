32505b803a4597928df51f5cc35d46f1914c0fab
luizvau <luizvau@gmail.com>
2021-03-23 02:18:31 -0300

    luiz

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,32 +1,52 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function LoginScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.logoC}>
+      <Image source={require('../assets/logo.png')} style={styles.logo}  />
+      </View>
+
+      <View style={styles.container}>
+      <Input placeholder='Nickname' leftIcon={<Icon name='user'size={24} color='#8F9EA6'/>}/>
+      <Input placeholder='Senha' leftIcon={<Icon name='lock'size={24} color='#8F9EA6'/>}/>
+      <Button title="Login" buttonStyle={{backgroundColor: "#0CB38A"}} />
+      <Button title="Cadastrar" type="clear" titleStyle={{color: "#60CD91"}} />      
+      </View>
+    </KeyboardAvoidingView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  logoC: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: '#fff',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    width: 340,
+    height: 340,
+    alignItems: 'stretch',
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    alignItems: 'baseline',
+    resizeMode: 'contain',
   },
 });
